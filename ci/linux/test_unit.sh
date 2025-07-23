aeeff5e62871436ea65e84f2ec120677f7eacde7
Jesús A. Rodríguez <jesusandres.rodriguez@deltares.nl>
2025-07-23 22:48:10 +0200

    install dependencies in CI

--- ci/linux/test_unit.sh
+++ ci/linux/test_unit.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 . .venv/bin/activate
+
+# Install the package in development mode
+pip install -e .
+
 PYTHONPATH='$PYTHONPATH:src/' pytest --junit-xml=test-results.xml unit_test/
--- ci/linux/typecheck.sh
+++ ci/linux/typecheck.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 . .venv/bin/activate
+
+# Install the package in development mode with dev dependencies
+pip install -e ".[dev]"
+
 python -m mypy ./src/kpicalculator ./unit_test/
